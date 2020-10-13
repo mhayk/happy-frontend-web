dfba903cfbf3f483400d1f93aa2d99909a88ee4a
Mhayk Whandson <hi@mhayk.com>
2020-10-13 13:32:11 +0100

    The initial page has been styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiArrowRight  } from 'react-icons/fi'
+
+import './styles/global.css';
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo"/>
+
+        <main>
+          <h1>Bring happiness to the world</h1>
+          <p>Visit orphanages and change many children's day.</p>
+        </main>
+
+        <div className="location">
+          <strong>United Kingdom</strong>
+          <span>London</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }
