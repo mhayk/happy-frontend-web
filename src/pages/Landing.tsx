3bc0e690f647cefeeae86c354788d3c9f1812aca
Mhayk Whandson <hi@mhayk.com>
2020-10-14 22:46:40 +0100

    The navigation to the page /app using Link from react-router-dom has been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom'
+
 import { FiArrowRight  } from 'react-icons/fi'
 
 import '../styles/pages/landing.css'
@@ -22,9 +24,9 @@ function Landing() {
           <span>London</span>
         </div>
 
-        <a href="" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
-        </a>
+        </Link>
       </div>
     </div>
     )
