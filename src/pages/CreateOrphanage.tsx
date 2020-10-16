e18e69906d90cb7cdcb8a6d883093df548b3b7ac
Mhayk Whandson <hi@mhayk.com>
2020-10-16 22:38:16 +0100

    The function mapIcon has been created to be use global.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
 
 import { FiPlus } from "react-icons/fi";
 
-import mapMarkerImg from '../images/map-marker.svg';
-
 import '../styles/pages/create-orphanage.css';
 import Sidebar from "../components/Sidebar";
-
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+import mapIcon from "../utils/mapIcon";
 
 export default function CreateOrphanage() {
   return (
@@ -27,16 +17,16 @@ export default function CreateOrphanage() {
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
+            <Map
+              center={[-27.2092052,-49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
             >
-              <TileLayer 
+              <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
             </Map>
 
             <div className="input-block">
